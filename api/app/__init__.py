d33ab6769b7d592577257758c09b037ac3509f7d
HargovindArora <hargovinds50@gmail.com>
2021-04-26 12:35:12 +0530

    Added testing suite and configuration and logging configuration

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import logging.config
+
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
@@ -6,11 +9,16 @@ from flask_jwt_extended import JWTManager
 
 from .routes.user_routes import initialize_user_routes
 from .routes.model_routes import initialize_model_routes
+
 from .database.db import initialize_db
+from .database.models import TokenBlocklist
 
 
 app = Flask(__name__)
 
+
+logging.config.fileConfig(fname="logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app.config.from_object("config.DevelopmentConfig")
 
@@ -26,3 +34,7 @@ initialize_db(app)
 
 bcrypt = Bcrypt(app)
 
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    return TokenBlocklist.is_jti_blacklisted(jti)
